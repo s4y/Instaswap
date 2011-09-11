af3bc2e549c81bf9be9b7e08a60ee861ead41715
Sidney San Martín <s@sidneysm.com>
2011-09-11 16:26:55 -0400

    Initial commit. Hackity McHackerson.

---
+++ bookmarklet.js
@@ -0,0 +1,23 @@
+(function() {
+	var d = document,
+		b = d.body,
+		l,
+		e = encodeURIComponent,
+		ce = 'createElement',
+		ac = 'appendChild',
+		de = 'documentElement',
+		q = prompt("Instaswap\n\nSend this page to…");
+	if (q) {
+		if (!b) {
+			b = d[ce]('body');
+			if (!d[de]) {
+				return;
+			}
+			d[de][ac](b);
+		}
+		l = d[ce]('scr' + 'ipt');
+		l.setAttribute('src', 'http://instaswap.sidnicio.us/api?key=api_key&target=' + e(q) + '&url=' + e(d.location.href));
+		b[ac](l);
+	}
+})();
+void(0);
